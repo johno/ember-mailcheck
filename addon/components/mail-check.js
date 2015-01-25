2c8ea88c4404ceab2b9fabfe2d3cf24355afc64b
John Otander <johnotander@gmail.com>
2015-01-25 14:50:20 -0700

    Tie mailcheck action into focus-out

--- addon/components/mail-check.js
+++ addon/components/mail-check.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  type: 'email',
+  name: 'email',
+  value: '',
+  placeholder: '',
+  classNames: ['mailcheck'],
 
+  actions: {
+    mailcheck: function() {
+      console.log('ember-mailcheck action called');
+    }
+  }
 });
